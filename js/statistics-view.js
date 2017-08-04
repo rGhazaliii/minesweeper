8c8dab239d2d95664f2a17b80b309a3972689649
r.Ghazali <rghazaliii@yahoo.com>
2017-08-04 23:54:42 +0430

    Show date bug fixed#2

--- js/statistics-view.js
+++ js/statistics-view.js
@@ -74,7 +74,7 @@
         var date = new Date(timestamp);
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
-        var day = date.getDay();
+        var day = date.getDate();
 
         return year + '/' + month + '/' + day;
     };
